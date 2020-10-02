4fcd401bc40fa0147b92283a0e1841bfa2cc163b
Scott Zetlan <szetlan@zola.com>
2020-10-02 14:23:43 +0000

    Separate host and vagrant install sets.

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function is_vagrant {
+    local hostname=`hostname`
+    if test "$hostname" = "dev.zola"; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 function is_installed {
     if test -f $1 && grep -q DOTFILES $1; then
         return 0
@@ -19,20 +28,25 @@ function install {
     fi
 }
 
-install $HOME/.bashrc bashrc
-install $HOME/.zsh_functions zsh_functions
-install $HOME/.exrc exrc
-install $HOME/.oh-my-zsh/custom/itermocil-auto-complete.zsh itermocil/itermocil-auto-complete.zsh
-
-if test -d "$HOME/.itermocil"; then
-    echo "Itermocil directory already exists"
+if is_vagrant; then
+    echo "*** Installing to vagrant"
+    install $HOME/.bashrc bashrc
 else
-    echo "Creating Itermocil directory"
-    mkdir $HOME/.itermocil
+    ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
+    echo "*** Installing to host"
+    cp zsh_functions $ZSH_CUSTOM/dotfiles.zsh
+    install $ZSH_CUSTOM/itermocil-auto-complete.zsh itermocil/itermocil-auto-complete.zsh
+    if test -d "$HOME/.itermocil"; then
+        echo "Itermocil directory already exists"
+    else
+        echo "Creating Itermocil directory"
+        mkdir $HOME/.itermocil
+    fi
+
+    echo "Refreshing itermocil configs"
+    cp itermocil/*.yml $HOME/.itermocil
 fi
 
-echo "Refreshing itermocil configs"
-cp itermocil/*.yml $HOME/.itermocil
-
-git config --global core.excludesFile "$PWD/gitignore"
+install $HOME/.exrc exrc
+git config --global core.excludesFile "$WEB_DIR/dotfiles/gitignore"
 
